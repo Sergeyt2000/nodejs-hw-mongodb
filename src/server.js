b285a5838522d06b547dc4d2e2809228aaa2ea61
Sergeyt2000 <sergeyt2000@gmail.com>
2025-07-07 16:56:12 +0300

    Refactored steps 1,2

--- src/server.js
+++ src/server.js
@@ -3,13 +3,17 @@ import cors from 'cors';
 import pino from 'pino-http';
 import dotenv from 'dotenv';
 import { getEnvVariable } from './utils/getEnvVariable.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import ContactsRouter from './routers/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+// import { getAllContacts, getContactById } from './services/contacts.js';
 
 dotenv.config();
 const PORT = getEnvVariable('PORT') || 3000;
 
 export function setupServer() {
   const app = express();
+  app.use(express.json());
   app.use(cors());
 
   app.use(
@@ -24,39 +28,44 @@ export function setupServer() {
     res.json({ message: 'Welcome to contacts DataBase!' });
   });
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
+  // app.get('/contacts', async (req, res) => {
+  //   const contacts = await getAllContacts();
+  //   res.json({
+  //     status: 200,
+  //     message: 'Successfully found contacts!',
+  //     data: contacts,
+  //   });
+  // });
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    const contactId = req.params.contactId;
-    console.log(`Fetching contact with ID: ${contactId}`);
-    const contact = await getContactById(contactId);
+  // app.get('/contacts/:contactId', async (req, res) => {
+  //   const contactId = req.params.contactId;
+  //   console.log(`Fetching contact with ID: ${contactId}`);
+  //   const contact = await getContactById(contactId);
 
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
-      });
-      return;
-    }
+  //   if (!contact) {
+  //     res.status(404).json({
+  //       message: 'Contact not found',
+  //     });
+  //     return;
+  //   }
 
-    res.json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
+  //   res.json({
+  //     status: 200,
+  //     message: `Successfully found contact with id ${contactId}!`,
+  //     data: contact,
+  //   });
+  // });
 
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+  app.use(ContactsRouter);
+
+  // app.use((req, res, next) => {
+  //   res.status(404).json({
+  //     message: 'Not found',
+  //   });
+  // });
+  app.use(notFoundHandler);
+
+  app.use(errorHandler);
 
   app.listen(PORT, (error) => {
     if (error) {
